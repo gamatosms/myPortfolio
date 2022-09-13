60755140e258ae0970b1135f06d70c282936fd80
Gabriel Matos Marques Silva <gsilva505@hotmail.com>
2022-09-13 16:29:35 -0300

    Alterando serviço de e-mail

--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -1,20 +1,32 @@
 import React from "react";
 import './contact.css'
 import { AiFillLinkedin, AiOutlineInstagram } from 'react-icons/Ai'
+import emailjs from "@emailjs/browser"
 
 export default function Contact() {
+
+    function sendEmail(e) {
+        e.preventDefault();
+        // these IDs from the previous steps
+        emailjs.sendForm("service_m72338m", "template_os8nmhe", e.target, "Guxq_2nJBsHyIEr0I")
+            .then(function () {
+                alert('SUCCESS!');
+            }, function (error) {
+                alert('FAILED...', error);
+            });
+        e.target.reset();
+    }
+
     return (
         <div className="contact" id="contact">
             <h3 className="pageTitle">Contact</h3>
             <main>
                 <div>
                     <h1 className="mainTitle">Reach me out!</h1>
-                    <form className="contactForm" action="https://formsubmit.co/gsilva505.gs@gmail.com" method="POST">
-                        <input placeholder="Name:" name="text" type="text" required />
-                        <input type="hidden" name="_subject" value="New submission in your site!" />
+                    <form className="contactForm" onSubmit={sendEmail}>
+                        <input placeholder="Name:" name="name" type="text" required />
                         <input placeholder="E-mail:" name="email" type="email" required />
-                        <input type="hidden" name="_captcha" value="false" />
-                        <textarea name="messageArea" placeholder="Type your message" id="message" cols="40" rows="10" required />
+                        <textarea name="message" placeholder="Type your message" id="message" cols="40" rows="10" required />
                         <button type="submit">Send</button>
                     </form>
                     <hr />
