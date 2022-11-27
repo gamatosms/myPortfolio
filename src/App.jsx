4b53c101a91f4d9f0295aae36304f4854bc6c3c0
Gabriel Matos Marques Silva <gabriel.silvam@sonda.com>
2022-11-27 20:07:36 -0300

    Arrumando component navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
-import MobileNav from './components/Navbar/mobileNav'
 import Footer from './components/Footer/Footer'
 import Home from './views/Home/Home'
 import About from './views/About/About'
@@ -10,7 +9,6 @@ import Contact from './views/Contact/Contact'
 function App() {
   return (
     <div>
-      <MobileNav />
       <Navbar />
       <Home />
       <About />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import './navbar.css'
 import { AiOutlineHome, AiOutlineMessage } from '/node_modules/react-icons/ai'
 import { BsPerson } from 'react-icons/bs'
 import { RiComputerLine } from 'react-icons/ri'
+import { BiMenuAltRight } from 'react-icons/bi'
+import { CgClose } from 'react-icons/cg'
+import { motion } from "framer-motion"
 
 export default function Navbar() {
+
+    const [isOpen, setIsOpen] = useState(false)
+    const hamburguerIcon = <BiMenuAltRight className="hamburguer"
+        size='50px' onClick={() => setIsOpen(!isOpen)} />
+    const closeIcon = <CgClose className="close"
+        size='50px' onClick={() => setIsOpen(!isOpen)} />
+    const closeMbNav = () => setIsOpen(false)
+    const animateFrom = { opacity: 0, y: -20 }
+    const animateTo = { opacity: 1, y: 0 }
+
     return (
+        <div>
         <div className="navbar">
             <nav>
                 <ul>
@@ -15,6 +29,28 @@ export default function Navbar() {
                     <li><a href="#contact"><AiOutlineMessage size="33px" color="white" /></a></li>
                 </ul>
             </nav>
+        </div>
+        <div>
+            <nav className="mb__nav">
+            {isOpen ? closeIcon : hamburguerIcon}
+            {isOpen &&
+                <ul onClick={closeMbNav}>
+                    <motion.li initial={animateFrom} animate={animateTo} transition={{ delay: 0.20 }}>
+                        <a href="#home">Home</a>
+                    </motion.li>
+                    <motion.li initial={animateFrom} animate={animateTo} transition={{ delay: 0.25 }}>
+                        <a href="#about">About</a>
+                    </motion.li>
+                    <motion.li initial={animateFrom} animate={animateTo} transition={{ delay: 0.30 }}>
+                        <a href="#projects">Projects</a>
+                    </motion.li>
+                    <motion.li initial={animateFrom} animate={animateTo} transition={{ delay: 0.35 }}>
+                        <a href="#contact">Contact</a>
+                    </motion.li>
+                </ul>
+            }
+        </nav>
+        </div>
         </div>
     )
 }
